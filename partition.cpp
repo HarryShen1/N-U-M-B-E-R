afdf612312ceb800e5bc2a2f02998200a2d6ca68
dragondoodler222 <dashiellb100@gmail.com>
2024-04-13 02:30:09 -0400

    Some updated scaffolding, work tbd

--- partition.cpp
+++ partition.cpp
@@ -10,22 +10,31 @@ using namespace std;
 using namespace std::chrono;
 
 default_random_engine generator;
+uniform_int_distribution<int> random_positive_integer(0, RAND_MAX);
+
 // ^ I think we're gonna need randomness eventually, just putting this in for now
 
 // Helper Code
 int *generate_random_prepartition(int n) {
+    // Random list of numbers 1 through n of length n
     return nullptr; // TODO
 }
 
 int *generate_random_prepartition_move(int *source, int n) {
+    // Take random i, j with source[i] != j, then set source[i] = j
     return nullptr; // TODO
 }
 
 int *generate_random_solution(int n) {
+    // Returns a random list of length n 
+    // List consists of +1 and -1, signs of the solution
     return nullptr; // TODO
 }
 
 int *generate_random_move(int *source, int n) {
+    // Take two random indices i, j with i \neq j
+    // flip i 
+    // flip j with probability .5
     return nullptr; // TODO
 }
 
